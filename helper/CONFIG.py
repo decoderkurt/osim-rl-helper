93194f31dfa7b066fa8d3ff4ecd32ea925a9de95
kurt <callmekoo@naver.com>
2018-08-06 21:03:44 +0900

    reg

--- helper/CONFIG.py
+++ helper/CONFIG.py
@@ -1,2 +1,2 @@
 remote_base = 'http://grader.crowdai.org:1729'
-crowdai_token = ''
+crowdai_token = 'c4cda3976f22b8f468b78a33e47bb432'
--- helper/baselines/keras/KerasDDPGAgent.py
+++ helper/baselines/keras/KerasDDPGAgent.py
@@ -18,52 +18,55 @@ class KerasDDPGAgent(KerasAgent):
     https://arxiv.org/abs/1509.02971
     """
     def __init__(self, observation_space, action_space, filename='KerasDDPGAgent.h5f'):
+        from tensorflow.python.client import device_lib
+        print(device_lib.list_local_devices())
         nb_actions = action_space.shape[0]
+        import keras.backend.tensorflow_backend as K
+        with K.tf.device('/gpu:0'):
+            # Actor network
+            actor = Sequential()
+            actor.add(Flatten(input_shape=(1,) + observation_space.shape))
+            actor.add(Dense(32))
+            actor.add(Activation('relu'))
+            actor.add(Dense(32))
+            actor.add(Activation('relu'))
+            actor.add(Dense(32))
+            actor.add(Activation('relu'))
+            actor.add(Dense(nb_actions))
+            actor.add(Activation('sigmoid'))
+            print(actor.summary())
 
-        # Actor network
-        actor = Sequential()
-        actor.add(Flatten(input_shape=(1,) + observation_space.shape))
-        actor.add(Dense(32))
-        actor.add(Activation('relu'))
-        actor.add(Dense(32))
-        actor.add(Activation('relu'))
-        actor.add(Dense(32))
-        actor.add(Activation('relu'))
-        actor.add(Dense(nb_actions))
-        actor.add(Activation('sigmoid'))
-        print(actor.summary())
+            # Critic network
+            action_input = Input(shape=(nb_actions,), name='action_input')
+            observation_input = Input(shape=(1,) + observation_space.shape, name='observation_input')
+            flattened_observation = Flatten()(observation_input)
+            x = concatenate([action_input, flattened_observation])
+            x = Dense(64)(x)
+            x = Activation('relu')(x)
+            x = Dense(64)(x)
+            x = Activation('relu')(x)
+            x = Dense(64)(x)
+            x = Activation('relu')(x)
+            x = Dense(1)(x)
+            x = Activation('linear')(x)
+            critic = Model(inputs=[action_input, observation_input], outputs=x)
+            print(critic.summary())
 
-        # Critic network
-        action_input = Input(shape=(nb_actions,), name='action_input')
-        observation_input = Input(shape=(1,) + observation_space.shape, name='observation_input')
-        flattened_observation = Flatten()(observation_input)
-        x = concatenate([action_input, flattened_observation])
-        x = Dense(64)(x)
-        x = Activation('relu')(x)
-        x = Dense(64)(x)
-        x = Activation('relu')(x)
-        x = Dense(64)(x)
-        x = Activation('relu')(x)
-        x = Dense(1)(x)
-        x = Activation('linear')(x)
-        critic = Model(inputs=[action_input, observation_input], outputs=x)
-        print(critic.summary())
+            # Setup Keras RL's DDPGAgent
+            memory = SequentialMemory(limit=100000, window_length=1)
+            random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.2,
+                                                      size=nb_actions)
+            self.agent = DDPGAgent(nb_actions=nb_actions,
+                              actor=actor,
+                              critic=critic,
+                              critic_action_input=action_input,
+                              memory=memory,
+                              nb_steps_warmup_critic=100,
+                              nb_steps_warmup_actor=100,
+                              random_process=random_process,
+                              gamma=.99,
+                              target_model_update=1e-3,
+                              delta_clip=1.)
+            self.agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
 
-        # Setup Keras RL's DDPGAgent
-        memory = SequentialMemory(limit=100000, window_length=1)
-        random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.2,
-                                                  size=nb_actions)
-        self.agent = DDPGAgent(nb_actions=nb_actions,
-                          actor=actor,
-                          critic=critic,
-                          critic_action_input=action_input,
-                          memory=memory,
-                          nb_steps_warmup_critic=100,
-                          nb_steps_warmup_actor=100,
-                          random_process=random_process,
-                          gamma=.99,
-                          target_model_update=1e-3,
-                          delta_clip=1.)
-        self.agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
-
-        self.filename = filename
+            self.filename = filename
